d0f321befd1475db07983d198d0d532a563bd66e
Jay Kim <djayshine87@hotmail.com>
2020-04-19 21:34:50 +1000

    next and previous songs loading

--- script.js
+++ script.js
@@ -2,40 +2,64 @@ console.log('lyrics from lyrics.ovh');
 const user_input = document.getElementById('user_input');
 
 const results=[]
-const second_results =[]
-
-let next_url='';
+const next_results=[]
 
 function find_song(term){
     fetch(`https://api.lyrics.ovh/suggest/${term}`)
     .then(res=>res.json())
     .then(data=>{
-        console.log(data);
-        console.log(data.next)
-        next_url = data.next
+        console.log(data)
         data.data.forEach(a=>results.push(a))
         display_results(results)
-        more_songs();
+
+        button = document.createElement('button');
+        button.textContent = 'NEXT';
+        document.getElementById('display').appendChild(button);
+
+        button.addEventListener('click',()=>{
+            more_songs(data.next);
+        })
     })
 }
 
-function more_songs(){
+
+function more_songs(next_url){
     fetch(`https://cors-anywhere.herokuapp.com/${next_url}`)
     .then(res=>res.json())
     .then(data=>{
-        console.log(data);
-        data.data.forEach(a=>second_results.push(a))
+        console.log(data)
+        data.data.forEach(a=>results.push(a))
+        display_results(results)
+
+        
+        if(data.prev){
+            button = document.createElement('button');
+            button.textContent = 'PREV';
+            document.getElementById('display').appendChild(button);
+
+            button.addEventListener('click',()=>{
+                more_songs(data.prev);
+            })
+        }
+
+        if(data.next){
+            button = document.createElement('button');
+            button.textContent = 'NEXT';
+            document.getElementById('display').appendChild(button);
+
+            button.addEventListener('click',()=>{
+                more_songs(data.next);
+            })
+        }
+
     })
 }
 
+
 function display_results(stuff_to_display){
-    console.log('should be cleared');
     document.getElementById('display').innerHTML = '';
     user_input.value = '';
-    const x = stuff_to_display;
-
-    const button = document.createElement('button');
-    x==results?button.innerHTML='Next':button.innerHTML='Previous'
+    
 
     stuff_to_display.forEach(a=>{
         const p = document.createElement('p');
@@ -46,24 +70,26 @@ function display_results(stuff_to_display){
         
     })
 
-    document.getElementById('display').appendChild(button)
 
-    button.addEventListener('click',()=>{
-        x==results?display_results(second_results):display_results(results)
-    })
     
+    next_url='';
+    prev_url='';
+    results.splice(0,results.length)
 }
+
+
+
+
+
+
+
 
 document.getElementById('my_form').addEventListener('submit',(e)=>{
     e.preventDefault();
-    
     const user_search = user_input.value.trim();
-
     if(!user_search){
         alert("can't leave it blank")
     }else{
         find_song(user_search)
     }
-    results.splice(0,results.length)
-    second_results.splice(0,second_results.length)
 })
